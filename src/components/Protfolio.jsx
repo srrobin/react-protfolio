20007ce46bc84d5469fe444cb38bdf175a208d24
“srrobin” <srrobin07@gmail.com>
2024-07-31 15:51:50 +0600

    up

--- src/components/Protfolio.jsx
+++ src/components/Protfolio.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import React from 'react';
-import protImage from "../assets/hero.jpg";
+// import protImage from "../assets/hero.jpg";
+import protImage from "../assets/p31.png";
 import Abouts from './Abouts';
 import AnimatedTextCharacter from './AnimatedTextCharacter';
 // import Artistic from './Artistic';
